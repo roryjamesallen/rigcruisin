7466df9b18b779ae17a0b3130609ac09aaf64f9d
Rory Allen <rory@hogwild.uk>
2026-05-29 07:58:43 +0100

    update index and add deploy script

--- index.php
+++ index.php
@@ -2,6 +2,29 @@
     <head>
     </head>
     <body>
-	rig cruisin
+	<span id="game-state"></span>
     </body>
 </html>
+
+<script>
+ // Game states: NOT_STARTED, DRIVING, INTERRUPTED, ENDED
+ 
+ const game_state_span = document.getElementById('game-state');
+ 
+ const game = {
+     state: 0,
+     interrupt_queue: [],
+     startup_s: 3,
+     startAnimation: function(secs) {
+	 for (let count=secs; count>0; --count){
+	     setTimeout(() => { game_state_span.innerText = count; }, (secs-count)*1000);
+	 }
+     },
+     startGame: function() {
+	 this.startAnimation(this.startup_s);
+	 setTimeout(() => { game_state_span.innerText = 'GAME ON'; }, (this.startup_s*1000));
+     }
+ }
+
+ game.startGame();
+</script>
---
+++ scripts/deploy.sh
@@ -0,0 +1,1 @@
+ssh -p 9284 wildhog@hogwild.uk "cd rigcruisin;git pull"
